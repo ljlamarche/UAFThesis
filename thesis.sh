434b332b99810e17cef8988df39047228f86db93
Leslie Lamarche <lesliejlamarche@gmail.com>
2017-01-04 10:47:06 -0900

    Incorporated ISR map

--- thesis.sh
+++ thesis.sh
@@ -3,8 +3,9 @@
 
 gs -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=530 -dDEVICEHEIGHTPOINTS=410 -sOutputFile=/Users/ljlamarche/Desktop/UAFThesis/Figures/densprofile.pdf /Users/ljlamarche/Desktop/UAFThesis/Figures/densprofile.ps
 
-gs -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=270 -dDEVICEHEIGHTPOINTS=520 -sOutputFile=/Users/ljlamarche/Desktop/UAFThesis/Figures/superdarnmap.pdf /Users/ljlamarche/Desktop/UAFThesis/Figures/SuperDARNmap.ps
+gs -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=270 -dDEVICEHEIGHTPOINTS=520 -sOutputFile=/Users/ljlamarche/Desktop/UAFThesis/Figures/SuperDARNmap.pdf /Users/ljlamarche/Desktop/UAFThesis/Figures/SuperDARNmap.ps
 
+gs -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=510 -dDEVICEHEIGHTPOINTS=510 -sOutputFile=/Users/ljlamarche/Desktop/UAFThesis/Figures/ISRmap.pdf /Users/ljlamarche/Desktop/UAFThesis/Figures/ISRmap.ps
 
 
 pdflatex LLamarcheThesis.tex
